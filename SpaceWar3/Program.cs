26c775d6d5d2294fbe3c0052c17ce7c161386821
Matt Watson <mattwatson1@gmail.com>
2013-08-19 01:04:29 +0100

    Renamed Game1 to SpaceWar3

--- SpaceWar3/Program.cs
+++ SpaceWar3/Program.cs
@@ -18,7 +18,7 @@ namespace SpaceWar3
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new SpaceWar3())
                 game.Run();
         }
     }
