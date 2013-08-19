8fb1abab5dac403f7750e04e4918091f17b82925
David Emmerson <david.emmerson@gmail.com>
2013-08-19 09:01:13 +0100

    Added Tickspec test project

---
+++ SpaceWar3.Tests/GameStepDefinitions.fs
@@ -0,0 +1,17 @@
+﻿namespace DEMW.SpaceWar3.Tests
+
+module GameStepDefinitions =
+
+    open TickSpec
+    open NUnit.Framework
+
+    
+
+    let [<Given>] ``I am using the default configuration`` () = 
+        ()
+      
+    let [<When>] ``I start a new instance of the program`` () =  
+        ()
+      
+    let [<Then>] ``it should be full screen`` () =     
+        ()
