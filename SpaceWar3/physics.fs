393e78d810d1be210ab1d854a91bb767c78f956d
Matt Watson <mattwatson1@gmail.com>
2013-09-11 19:29:26 +0100

    Added Physics module with Force

---
+++ SpaceWar3/physics.fs
@@ -0,0 +1,11 @@
+﻿module DEMW.SpaceWar3.Physics
+
+open Microsoft.Xna.Framework
+
+type Force = { Vector : Vector2; Displacement : Vector2 }
+
+let rotateForce (force : Force) (angle : float) =
+    let rotation = Matrix.CreateRotationZ(float32 angle)
+    let vector = Vector2.Transform(force.Vector, rotation)
+    let displacement = Vector2.Transform(force.Displacement, rotation)
+    { Vector = vector; Displacement = displacement }
