a5a138623b880dbb1e9b2935de66dd723ae5f1d2
David Emmerson <david.emmerson@gmail.com>
2013-12-27 21:13:11 +0000

    Took out custom mono. Removed mono tests.

--- SpaceWar3.Tests/GameStepDefinitions.fs
+++ SpaceWar3.Tests/GameStepDefinitions.fs
@@ -6,23 +6,5 @@ module GameStepDefinitions =
     open NUnit.Framework
     open DEMW.SpaceWar3
 
-    let mutable game = None
-
-    let [<Given>] ``I am using the default configuration`` () = 
-        ()
-      
-    let [<When>] ``I start a new instance of the program`` () =  
-        game <- Some <| new SpaceWar3()
-        
-        match game with
-            
-            | Some g -> do
-                g.Run()
-            
-            | _ -> Assert.Fail("could not create instance of SpaceWar3")
-      
-    let [<Then>] ``it should be full screen`` () =
-        match game with
-            | Some g -> do     
-                g.Dispose()
-            | _ -> Assert.Fail("no game was created")
+    let [<Then>] ``fail`` () =
+        Assert.Fail()
